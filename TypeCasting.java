59a988cfebea203195c7637f0823cd73d66cc27d
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-08-26 17:24:58 +0530

    Task-2 Q-2

---
+++ TypeCasting.java
@@ -0,0 +1,15 @@
+package com.core;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		double orderTotal=349.75;
+		int couponAmount=(int) orderTotal;
+		
+		System.out.println("Order Total:"+orderTotal);
+		System.out.println("coupon Eligibility Amount"+couponAmount);
+		
+		
+	
+	}
+
+}
