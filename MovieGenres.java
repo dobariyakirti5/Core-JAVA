355afe1baf62352e0b7437ffbe7a845eb086aff3
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-01 17:38:05 +0530

    T-3 Q-3

---
+++ MovieGenres.java
@@ -0,0 +1,12 @@
+package com.core;
+
+public class MovieGenres {
+	public static void main(String[] args) {
+		String genres[]= {"Action","Comedy","Drama","Horror","Sci=Fi"};
+		
+		for(int i=0;i<genres.length;i++) {
+			System.out.println(genres[i]);
+		}
+	}
+
+}
