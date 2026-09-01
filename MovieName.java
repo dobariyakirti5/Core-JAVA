c1595df0051702a66adb576cb61569a754bde841
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-01 18:11:43 +0530

    T-4 Q-1

---
+++ MovieName.java
@@ -0,0 +1,13 @@
+package com.core;
+
+public class MovieName {
+	public static void main(String[] args) {
+		String MovieNames[]= {"KFG","Saiyara","KGF2","RRR","Pushpa"};
+		for(int i=0;i<MovieNames.length;i++) {
+			System.out.println(MovieNames[i]);
+		}
+				
+		
+	}
+
+}
