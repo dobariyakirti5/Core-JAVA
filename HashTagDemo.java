de51472723dd2171e3692674d2dbf9bc9d4c7ded
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-06 18:22:33 +0530

    T-7 Q-5

---
+++ HashTagDemo.java
@@ -0,0 +1,21 @@
+package com.core;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashTagDemo {
+	public static void main(String[] args) {
+        Set<String> hashtags = new HashSet<>();
+
+        hashtags.add("#Java");
+        hashtags.add("#Testing");
+        hashtags.add("#Java");
+        hashtags.add("#Selenium");
+        hashtags.add("#Testing");
+
+        System.out.println(hashtags);
+}
+
+}
+	
+
+
