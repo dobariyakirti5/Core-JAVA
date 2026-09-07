639ab26603e046d9d18dab238f913fd8a5443576
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-07 17:49:12 +0530

    T-9 Q-3.3

---
+++ MyntraLoginPage.java
@@ -0,0 +1,16 @@
+package com.core;
+
+public class MyntraLoginPage {
+	 public void enterUsername() {
+	        System.out.println("Entering username");
+	    }
+
+	    public void enterPassword() {
+	        System.out.println("Entering password");
+	    }
+
+	    public void clickLoginButton() {
+	        System.out.println("Clicking login button");
+	    }
+
+}
