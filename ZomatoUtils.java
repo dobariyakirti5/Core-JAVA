664aa92379510bf05209f6a1b365a35161f069e1
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-07 18:00:35 +0530

    T-9 Q-5

---
+++ ZomatoUtils.java
@@ -0,0 +1,17 @@
+package com.core;
+
+public class ZomatoUtils {
+    public static void searchRestaurant(String name) {
+        System.out.println("Searching restaurant: " + name);
+    }
+
+    public static void selectLocation(String location) {
+        System.out.println("Selecting location: " + location);
+    }
+
+    public static void addItemToCart(String itemName) {
+        System.out.println("Adding item to cart: " + itemName);
+    }
+
+
+}
