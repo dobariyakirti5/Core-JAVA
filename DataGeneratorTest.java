5fc76d8f7cdb105221c30a53146c7da0e9abe6c5
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-07 10:21:09 +0530

    T-9 Q-2

---
+++ DataGeneratorTest.java
@@ -0,0 +1,15 @@
+package com.core;
+
+public class DataGeneratorTest {
+	 public static void main(String[] args) {
+
+	        String username = DataGenerator.generateUsername();
+
+	        String password = DataGenerator.generatePassword();
+
+	        System.out.println("Username = " + username);
+
+	        System.out.println("Password = " + password);
+	    }
+	}
+
