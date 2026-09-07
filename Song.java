0bfb362b856fd1e7f48559e035523ed8009b7d60
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-07 18:43:39 +0530

    T-10 Q-5

---
+++ Song.java
@@ -0,0 +1,18 @@
+package com.core;
+
+public class Song {
+	String title;
+    String artist;
+
+    public Song(String title, String artist) {
+
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String toString() {
+
+        return title + " - " + artist;
+    }
+
+}
