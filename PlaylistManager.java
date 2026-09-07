7508f1e280378e2cd8761e6852d9605056e18b6d
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-07 18:13:19 +0530

    T-10 Q-2

---
+++ PlaylistManager.java
@@ -0,0 +1,67 @@
+package com.core;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PlaylistManager {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> playlist = new ArrayList<String>();
+
+        int choice;
+
+        do {
+            System.out.println("\n1. Add Song");
+            System.out.println("2. View Playlist");
+            System.out.println("3. Remove Song");
+            System.out.println("4. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+            case 1:
+                System.out.print("Enter song name: ");
+                String song = sc.nextLine();
+
+                playlist.add(song);
+
+                System.out.println("Song added successfully!");
+                System.out.println("Playlist: " + playlist);
+                break;
+
+            case 2:
+                System.out.println("Playlist: " + playlist);
+                break;
+
+            case 3:
+                System.out.print("Enter song name to remove: ");
+                String removeSong = sc.nextLine();
+
+                if (playlist.remove(removeSong)) {
+                    System.out.println("Song removed successfully!");
+                } else {
+                    System.out.println("Song not found!");
+                }
+
+                System.out.println("Playlist: " + playlist);
+                break;
+
+            case 4:
+                System.out.println("Exiting Playlist Manager...");
+                break;
+
+            default:
+                System.out.println("Invalid choice!");
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+
+
+}
