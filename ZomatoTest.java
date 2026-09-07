ce614ea76cb56bb27a541a9a8a2f29ff111c03d8
dobariyakirti5 <dobariyakirti5@gmail.com>
2026-09-07 18:01:16 +0530

    T-9 Q-5.5

---
+++ ZomatoTest.java
@@ -0,0 +1,13 @@
+package com.core;
+
+
+	public class ZomatoTest {
+
+	    public static void main(String[] args) {
+
+	        ZomatoUtils.selectLocation("Ahmedabad");
+	        ZomatoUtils.searchRestaurant("Domino's");
+	        ZomatoUtils.addItemToCart("Margherita Pizza");
+	    }
+
+}
